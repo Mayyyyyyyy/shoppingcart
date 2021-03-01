e8a7545307f01c45ec825847a41986ef5c6900a6
Mayyyyyyyy <8633009+mayyyyyyyy@user.noreply.gitee.com>
2021-03-01 11:16:15 +0800

    move data

--- src/components/Show.js
+++ src/components/Show.js
@@ -1,4 +1,39 @@
-import showData from './ShoeData';
+import shoeone from '../images/1.png';
+import shoetwo from '../images/2.png';
+import shoethree from '../images/3.png';
+import shoefour from '../images/4.png';
+import shoefive from '../images/5.png';
+import shoesix from '../images/6.png';
+
+
+const showData =[
+    {
+        img:shoeone,
+        price:699
+    },
+    {
+        img: shoetwo,
+        price: 599
+    },
+    {
+        img: shoethree,
+        price: 499
+    },
+    {
+        img: shoefour,
+        price: 399
+    },
+    {
+        img: shoefive,
+        price: 299
+    },
+    {
+        img: shoesix,
+        price: 199
+    },
+]
+
+
 
 const Show = (props) => {
     const {updateCart} = props;
